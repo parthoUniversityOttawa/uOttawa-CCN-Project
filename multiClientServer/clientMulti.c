dd7da26bff09b6599269897413c693bed423f848
Partho Ghosal <partho.universities@gmail.com>
2019-09-25 10:34:12 -0400

    initialExample

---
+++ multiClientServer/clientMulti.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <fcntl.h> // for open
+#include <unistd.h> // for close
+#include<pthread.h>
+void * cientThread(void *arg)
+{
+  int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+  printf("clientSocket %d\n",clientSocket );
+  printf("In thread\n");
+  char message[1000];
+  char buffer[1024];
+ // int clientSocket;
+  struct sockaddr_in serverAddr;
+  socklen_t addr_size;
+ // printf("start!")  ;
+
+ // printf("End!")  ;
+
+  //Configure settings of the server address
+ // Address family is Internet 
+  serverAddr.sin_family = AF_INET;
+  //Set port number, using htons function 
+  serverAddr.sin_port = htons(7799);
+ //Set IP address to localhost
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+    //Connect the socket to the server using the address
+    addr_size = sizeof serverAddr;
+    connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+    perror("Reason");
+    strcpy(message,"I am Client");
+  if( send(clientSocket , message , strlen(message) , 0) < 0)
+    {
+            printf("Send failed\n");
+    }
+    //Read the message from the server into the buffer
+    if(recv(clientSocket, buffer, 1024, 0) < 0)
+    {
+       printf("Receive failed\n");
+    }
+   perror("rcv CALL:");
+    //Print the received message
+    printf("Data received: %s\n",buffer);
+    printf("clientSocket LAST %d\n",clientSocket );
+    close(clientSocket);
+    pthread_exit(NULL);
+}
+int main(){
+  int i = 0;
+  pthread_t tid[51];
+  pthread_t abc;
+
+  //pthread_create(&abc, NULL, cientThread, NULL);
+ //printf("done");
+ /// printf("end");
+
+ while(i< 10)
+  {
+    if( pthread_create(&tid[i], NULL, cientThread, NULL) != 0 )
+           printf("Failed to create thread\n");
+    i++;
+  }
+  sleep(20);
+  i = 0;
+  while(i< 10)
+  {
+     pthread_join(tid[i++],NULL);
+     printf("%d:\n",i);
+  }
+  printf("Finish");
+  return 0;
+}
